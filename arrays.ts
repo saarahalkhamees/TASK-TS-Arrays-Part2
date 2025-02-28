10e5e48b928d184559546eda3039f975cafbc7cd
TariqBazadough <tariq.bazadough@gmail.com>
2025-02-28 17:41:29 +0300

    I have changed the following: 1- function description fix 2- add function type declartion 3- add explantion to some of the examples

--- arrays.ts
+++ arrays.ts
@@ -1,53 +1,63 @@
 /**
  * isArrayLengthOdd(numbers):
- * - receives array numbers
+ * - Accepts a "numbers" parameter of type "number[]"
  * - returns true if array has an odd number of elements
  * - returns false otherwise
  *
  * e.g.
- * isArrayLengthOdd([1, 2, 3]) -> true
- * isArrayLengthOdd([1, 2, 3, 4]) -> flase
+ * isArrayLengthOdd([1, 2, 3]); // => true (the array contains 3 elements, 3 is odd, we return true)
+ * isArrayLengthOdd([1, 2, 3, 4]); // => false (the array contains 4 elements, 4 is even, we return false)
  */
-function isArrayLengthOdd(numbers) {
+
+function isArrayLengthOdd(numbers: []): boolean {
   // Your code here
+
+  return false; // replace false with what you see is fit
 }
 
 /**
  * isArrayLengthEven(numbers):
- * - receives array numbers
+ * - Accepts a "numbers" parameter of type "number[]"
  * - returns true if array has an even number of elements
  * - returns false otherwise
  *
  * e.g.
- * isArrayLengthEven([1, 2, 3]) -> false
- * isArrayLengthEven([1, 2, 3, 4]) -> true
+ * isArrayLengthEven([1, 2, 3]); // => false (the array contains 3 elements, 3 is odd, we return false)
+ * isArrayLengthEven([1, 2, 3, 4]); // => true (the array contains 4 elements, 4 is even, we return true)
  */
-function isArrayLengthEven(numbers) {
+
+function isArrayLengthEven(numbers: number[]): boolean {
   // Your code here
+
+  return false; // replace false with what you see is fit
 }
 
 /**
  * addLailaToArray(instructors):
- * - receives array instructors
+ * - Accepts a "instructors" parameter of type "string[]"
  * - returns a new array that's a copy of array instructors with additional string "Laila"
  *
  * e.g.
- * addLailaToArray(["Mshary", "Hasan"]) -> ["Mshary", "Hasan", "Laila"]
+ * addLailaToArray(["Mshary", "Hasan"]); // => ["Mshary", "Hasan", "Laila"]
  */
-function addLailaToArray(instructors) {
+function addLailaToArray(instructors: string[]): string[] {
   // Your code here
+
+  return []; // replace empty array with what you see is fit
 }
 
 /**
  * eliminateTeam(teams):
- * - receives array teams
+ * - Accepts a "teams" parameter of type "string[]"
  * - removes the last element from the array and return it
  *
  * e.g.
- * eliminateTeam(["Brazil", "Germany", "Italy"]) -> "Italy"
+ * eliminateTeam(["Brazil", "Germany", "Italy"]); // => "Italy"
  */
-function eliminateTeam(teams) {
+function eliminateTeam(teams: string[]): string {
   // Your code here
+
+  return ""; // replace empty string with what you see is fit 
 }
 
 export { isArrayLengthOdd, isArrayLengthEven, addLailaToArray, eliminateTeam };
